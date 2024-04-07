d3fd1f1a0b7871e3be3f1745444396c4004b5d7a
Catron-Gainey <catrong23@gmail.com>
2024-04-07 02:34:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("running")
+
+
+let janedoe = document.querySelector(".jd");
+
+function switchName() {
+janedoe.innerText = "John Doe";
+}
+
+
+let toddText = document.querySelector(".todd-text")
+function removeT() {
+    toddText.remove();
+}
+
+let philText = document.querySelector(".phil-text")
+function removeP() {
+    philText.remove();
+}
